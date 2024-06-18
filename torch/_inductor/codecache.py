19f689293ac5e3e7193000d85c8c140e86e6f143
Oguz Ulgen <oulgen@meta.com>
2024-06-18 15:33:43 -0700

    Evaluate symexprs on load path of cache not write
    ghstack-source-id: 221e8981ee5aebd1ae9cecc4550ee903246b2514
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128997

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -70,6 +70,8 @@ from torch.fx.experimental.symbolic_shapes import has_hint, hint_int, ShapeEnv
 if TYPE_CHECKING:
     from concurrent.futures import Future
 
+    import sympy
+
     from torch._inductor.graph import GraphLowering
     from torch._inductor.ir import ChoiceCaller
     from torch._inductor.runtime.hints import HalideMeta
@@ -1115,7 +1117,7 @@ class CompiledFxGraph:
     mutated_input_idxs: Set[int]
     constants: Dict[str, torch.Tensor]
     torchbind_constants: Dict[str, torch._C.ScriptObject]
-    output_strides: Optional[List[Optional[Tuple[int, ...]]]]
+    output_strides: Optional[List[Optional[Tuple[sympy.Expr, ...]]]]
     disabled_cudagraphs_reason: Optional[str]
     metrics_deltas: metrics.CachedMetricsDeltas
     # This is a string representation of an expression we serialize
@@ -1132,7 +1134,7 @@ class CompiledFxGraph:
         self,
         current_callable: Optional[Callable[..., Any]],
         graph: GraphLowering,
-        output_strides: List[Optional[Tuple[int, ...]]],
+        output_strides: List[Optional[Tuple[sympy.Expr, ...]]],
         disabled_cudagraphs_reason: Optional[str],
         metrics_deltas: metrics.CachedMetricsDeltas,
     ):
